fb423d40a51ffc1b0f317349479abe7718ac8958
Ank911007 <145314378+Ank911007@users.noreply.github.com>
2024-09-12 22:42:21 +0530

    Create command_line_line_argument.sh

---
+++ command_line_line_argument.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Script name : $0"
+echo "First argument : $1"
+echo "Second argument : $2"
+echo "Total no of argument : $#"
+echo "All argument as list : $@"
+echo "All argument as string: $*"
