b73e990fea6cf546432f359ff6ee368dcc805a1b
Ank911007 <145314378+Ank911007@users.noreply.github.com>
2024-09-12 20:58:39 +0530

    Create if_elif_else.sh

---
+++ if_elif_else.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+num=10
+
+if [ $num -gt 10 ]; then
+        echo "no is greater than 10"
+elif [ $num -eq 10 ]; then
+        echo "num is equal to 10"
+else
+        echo "no is less than 10"
+fi
