99040cee8dc5deccbce9e4076a9d0460627483cc
Ank911007 <145314378+Ank911007@users.noreply.github.com>
2024-09-13 13:32:54 +0530

    Create env_var.sh

---
+++ env_var.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Path : $PATH"
+echo "Home directory : $HOME"
+echo "Username : $USER"
+echo "Current directory : $PWD"
+echo "Language : $LANG"
+echo "primary prompt : $PS1"
+echo "bash path : $BASH"
+echo "bash version : $BASH_VERSION"
+echo "PID : $$"
+echo "Hostname : $HOSTNAME"
